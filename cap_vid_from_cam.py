6b38565cc1f3e76d874f8e7bca63084ab6554049
Alexander Mozglov <almozglov@gmail.com>
2020-06-07 00:49:45 +0300

    1st commit of my git repo for opencv tutorials

--- cap_vid_from_cam.py
+++ cap_vid_from_cam.py
@@ -1,13 +1,20 @@
+#Захватывает видео со встроенной камеры и выводит в небольшом 
+#окне в ЧБ
 import cv2
 import numpy as np
 
+#Инициализируем объект захвата изображения. вместо 0 можно
+#указать название видео файла
 cap = cv2.VideoCapture(0)
 
 while (True):
+	#cap.read выдаёт два значения: true при успешном захвате, и
+	# кадр 
 	ret, frame = cap.read()
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	cv2.imshow('frame', gray)
-	print(cap.get(3))
+	#Здесь важно указать время задержки (число). Без него
+	#захватывает только один кадр
 	if cv2.waitKey(2) & 0xFF == ord('a'):
 		break
 
